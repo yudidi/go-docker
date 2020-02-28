72c385a6f0909276189aaaa8832ea71866b132a3
yudidi <972656027@qq.com>
2020-02-28 17:30:10 +0800

    注释

--- command.go
+++ command.go
@@ -5,17 +5,18 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 	"go-docker/container"
+	"time"
 )
 
 // 创建namespace隔离的容器进程
 // 启动容器
 var runCommand = cli.Command{
-	Name: "run",
+	Name:  "run",
 	Usage: "Create a container with namespace and cgroups limit",
 	Flags: []cli.Flag{
 		cli.BoolFlag{
-			Name:        "ti",
-			Usage:       "enable tty",
+			Name:  "ti",
+			Usage: "enable tty",
 		},
 	},
 	Action: func(context *cli.Context) error {
@@ -31,11 +32,17 @@ var runCommand = cli.Command{
 
 // 初始化容器内容,挂载proc文件系统，运行用户执行程序
 var initCommand = cli.Command{
-	Name:                   "init",
-	Usage:                  "Init container process run user's process in container. Do not call it outside",
+	Name:  "init",
+	Usage: "Init container process run user's process in container. Do not call it outside",
 	Action: func(context *cli.Context) error {
 		logrus.Infof("init come on")
 		cmd := context.Args().Get(0)
-		return container.RunContainerInitProcess(cmd, nil)
+		fmt.Println(cmd)
+		for {
+			fmt.Println(time.Now())
+			time.Sleep(1 * time.Second)
+		}
+		return nil
+		//return container.RunContainerInitProcess(cmd, nil)
 	},
 }

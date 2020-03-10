8c1576c6d462482e9bb32bd4ea88926274d9c9a1
yudidi <972656027@qq.com>
2020-03-10 14:13:12 +0800

    测试没有mount proc的情况

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"os"
+	"os/exec"
+	"syscall"
 
 	"github.com/urfave/cli"
 )
@@ -13,11 +15,41 @@ func main() {
 	app.Usage = "implementation of mydocker"
 
 	app.Commands = []cli.Command{
-		command.RunCommand,
+		RunCommand,
 	}
 
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+var RunCommand = cli.Command{
+	Name: "run",
+	Flags: []cli.Flag{
+		cli.BoolFlag{
+			Name:  "it",
+			Usage: "enable tty",
+		},
+	},
+	Action: func(c *cli.Context) error {
+		tty := c.Bool("it")
+		command := c.Args().Get(0)
+		return Run(command, tty)
+	},
+}
+
+func Run(command string, tty bool) error {
+	cmd := exec.Command(command)
+	// for kinds of namespace
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWNET | syscall.CLONE_NEWIPC,
+	}
+
+	if tty {
+		cmd.Stderr = os.Stderr
+		cmd.Stdout = os.Stdout
+		cmd.Stdin = os.Stdin
+	}
+	return cmd.Run()
 }
